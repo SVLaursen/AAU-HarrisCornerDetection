24310e3df09ebb43c7a24a2fac54cbb28f19b4b1
Simon Vestergaard Laursen <simonvlaursen@gmail.com>
2021-03-28 20:24:20 +0200

    now functioning

--- hcd.py
+++ hcd.py
@@ -1,14 +1,88 @@
 import cv2 as cv
 import numpy as np
 import math
+import scipy.ndimage as ndimage
 
-def harris_corner_detection(src_img, k, window_size, threshold):
+
+def rgb2gray(img):
+    rgb_weights = [0.2989, 0.5870, 0.1140]
+    return np.dot(img[..., :3], rgb_weights)
+
+def gradient_sobel_filter(img):
+    img_x = ndimage.convolve(img, np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]))
+    img_y = ndimage.convolve(img, np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]]))
+
+    result_x = img_x / np.max(img_x)
+    result_y = img_y / np.max(img_y)
+    return result_x, result_y
+
+
+def harris_corner_detection(src_img, k, threshold, dist: 10):
     print('Starting Harris Corner Detection')
 
+    gray = rgb2gray(src_img)
+    blurred = ndimage.gaussian_filter(gray, sigma=1.0)
+    sobel_x, sobel_y = gradient_sobel_filter(blurred)
+
+    # FINDING CORNERS
+    print('Finding corners...')
+
+    xx = sobel_x * sobel_x
+    yy = sobel_y * sobel_y
+    xy = sobel_x * sobel_y
+
     corners = []
-    gray = cv.cvtColor(src_img, cv.COLOR_BGR2GRAY)
-    offset = int(window_size/2)
+    max_count = 0
 
-        
+    for i in range(1, int(src_img.shape[0] - 1)):
+        for j in range(1, int(src_img.shape[1] - 1)):
+            window_x = xx[i - 4: i + 5, j - 4: j + 5]
+            window_y = yy[i - 4: i + 5, j - 4: j + 5]
+            window_xy = xy[i - 4: i + 5, j - 4: j + 5]
 
-    return "bababoie"
+            sum_x = np.sum(window_x)
+            sum_y = np.sum(window_y)
+            sum_xy = np.sum(window_xy)
+
+            determinant = (sum_x * sum_y) - (sum_xy * sum_xy)
+            trace = sum_x + sum_y
+            r = determinant - (k * trace * trace)
+            corners.append((i, j, r))
+
+            if r > max_count:
+                max_count = r
+
+    # COMPARING TO THRESHOLD
+    print('Comparing corners to threshold...')
+
+    L = []
+
+    for res in corners:
+        i, j, r = res
+        if r > threshold:
+            L.append([i, j, r])
+
+
+    # NON-MAXIMAL SUPPRESSION
+    print('Performing non-maximal suppression...')
+
+    sorted_l = sorted(L, key=lambda x: x[2], reverse=True)
+    final = [sorted_l[0][:-1]]
+    xc, yc = [], []
+
+    for i in sorted_l:
+        for j in sorted_l:
+            if abs(i[0] - j[0] <= dist) and abs(i[1] - j[1] <= dist):
+                break
+            else:
+                final.append(i[:-1])
+                xc.append(i[1])
+                yc.append(i[0])
+
+    result = np.zeros(src_img.shape)
+
+    for i in final:
+        y, x = i[0], i[1]
+        result[y][x] = 1
+
+    return result
--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-import cv2
+import cv2 as cv
 import hcd
 
-filename = ""
-img = cv2.imread(filename)
-result = hcd.harris_corner_detection(img)
+# READ IMG FILE
+filename = "./res/imgs/shapes.png"
+img = cv.imread(filename)
 
-cv2.imshow('Result', result)
-if cv2.waitKey(0) & 0xff == 27:
-    cv2.destroyAllWindows()
+# USE HARRIS CORNER DETECTION
+result = hcd.harris_corner_detection(img, 0.1, 0.01, 0.001)
+
+# SHOW RESULTS
+cv.imshow('Original', img)
+cv.imshow('Result', result)
+
+# KEEP THE IMAGES AROUND TILL TOLD TO SHUTDOWN
+if cv.waitKey(0) & 0xff == 27:
+    cv.destroyAllWindows()
